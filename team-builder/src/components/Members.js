81e598606fd9bee1ffc16bb0af5e22fd088d24a7
Prem Puttegowda <puttegowdaprem@gmail.com>
2019-12-18 14:03:28 -0800

    Finished MVP(fixed the bug)

--- team-builder/src/components/Members.js
+++ team-builder/src/components/Members.js
@@ -4,19 +4,18 @@ function Members(props){
     console.log(props.teamMembers);
     if (!props.teamMembers) return (<div>No Members are added yet</div>)
     return (
-        <div >
+        <div className="team-container">
             {props.teamMembers.map( teamMember => (
-                <div key={teamMember.id} className="team-container">
+                <div key={teamMember.id} className="team-member">
                     
-                    <div className="team-member">
-                    {teamMember.name}
-                    </div>
-                    <div className="team-member">
+                    <h2>{teamMember.name}</h2>
+                    
+                    <p>
                         {teamMember.email}
-                    </div>
-                    <div className="team-member">
+                    </p>
+                    <p>
                         {teamMember.role}
-                    </div>
+                    </p>
                 </div>
             ))}
         </div>
